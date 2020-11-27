b87bfddef9757114bfef3eced511c9e34c700755
Liudmila-Sheremet <Sasha.sheremetka@gmail.com>
2020-11-27 16:44:29 +0000

    Initial code

--- js/carousel.js
+++ js/carousel.js
@@ -10,15 +10,18 @@ function slide() {
   currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
   showSlide(currentIdx);
 }
+function showSlide(idx) {
+  document.querySelector(".carousel img").src = images[idx];
+}
 setInterval(slide, 4000);
 function back() {
   currentIdx = --currentIdx < 0 ? images.length - 1 : currentIdx;
   showSlide(currentIdx);
 }
-document.querySelector(".carousel .btn-back").addEventListener("click", back);
-function showSlide(idx) {
-  document.querySelector(".carousel img").src = images[idx];
-}
+document
+.querySelector(".carousel .btn-back")
+.addEventListener("click", back);
+
 function forward() {
   currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
   showSlide(currentIdx);
@@ -26,6 +29,3 @@ function forward() {
 document
   .querySelector(".carousel .btn-forward")
   .addEventListener("click", forward);
-function showSlide(idx) {
-  document.querySelector(".carousel img").src = images[idx];
-}
