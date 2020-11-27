8ae3f27519c4f39279259c3589220a94200bb5db
Liudmila-Sheremet <Sasha.sheremetka@gmail.com>
2020-11-27 12:14:22 +0000

    css added

--- js/carousel.js
+++ js/carousel.js
@@ -1,30 +1,31 @@
-
 const images = [
-    'img/apricot.jpg',
-    'img/grapes.jpg',
-    'img/plum.jpg',
-    'img/raspberry.jpg',
-    'img/pear.jpg'
-]
+  "img/apricot.jpg",
+  "img/grapes.jpg",
+  "img/plum.jpg",
+  "img/raspberry.jpg",
+  "img/pear.jpg",
+];
 let currentIdx = 0;
 function slide() {
-    currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
-    showSlide(currentIdx);
+  currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
+  showSlide(currentIdx);
 }
 setInterval(slide, 4000);
 function back() {
-    currentIdx = --currentIdx < 0 ? images.length - 1 : currentIdx;
-    showSlide(currentIdx);
+  currentIdx = --currentIdx < 0 ? images.length - 1 : currentIdx;
+  showSlide(currentIdx);
 }
-document.querySelector('.carousel .btn-back').addEventListener('click', back);
+document.querySelector(".carousel .btn-back").addEventListener("click", back);
 function showSlide(idx) {
-    document.querySelector('.carousel img').src = images[idx];
+  document.querySelector(".carousel img").src = images[idx];
 }
 function forward() {
-    currentIdx = ++currentIdx > images.length - 1 ? 0: currentIdx;
-    showSlide(currentIdx);
+  currentIdx = ++currentIdx > images.length - 1 ? 0 : currentIdx;
+  showSlide(currentIdx);
 }
-document.querySelector('.carousel .btn-forward').addEventListener('click', forward);
+document
+  .querySelector(".carousel .btn-forward")
+  .addEventListener("click", forward);
 function showSlide(idx) {
-    document.querySelector('.carousel img').src = images[idx];
+  document.querySelector(".carousel img").src = images[idx];
 }
--- js/clock.js
+++ js/clock.js
@@ -1,3 +1,18 @@
-const clock = document.querySelector('.clock');
-setInterval( () => clock.innerText = new Date().toLocaleTimeString(), 1000  )
+const clock = document.querySelector(".clock");
+setInterval(() => (clock.innerText = new Date().toLocaleTimeString()), 1000);
 
+function zero_first_format(value) {
+  if (value < 10) {
+    value = "0" + value;
+  }
+  return value;
+}
+
+function date_time() {
+  const current_datetime = new Date();
+  const day = zero_first_format(current_datetime.getDate());
+  const month = zero_first_format(current_datetime.getMonth() + 1);
+  const year = current_datetime.getFullYear();
+  return day + "." + month + "." + year;
+}
+document.getElementById("current-date-block").innerHTML = date_time();
